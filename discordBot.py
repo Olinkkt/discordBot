5c85899f4207f2f8b94ea880ed966492fd6e6311
Olinkkt <130317910+Olinkkt@users.noreply.github.com>
2023-05-08 00:49:17 +0200

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -1,39 +1,44 @@
-import requests
 import discord
-from discord.ext import tasks
+import requests
 
-# Set up your Discord bot token
-TOKEN = "MTEwNDg5NzI0NDg5NDg1NTIxOQ.GHZZxK.pU5IjWbfS1P7zonx-VW6k8idqKAFFD3bJWk13g
-"
+API_URL = "http://ergast.com/api/f1/current/last/results.json"
 
-# Define a function to retrieve the F1 race results and post them in the Discord channel
-def post_race_results():
-    # Retrieve the race results from the Ergast API
-    response = requests.get("https://ergast.com/api/f1/current/last/results.json")
-    results = response.json()["MRData"]["RaceTable"]["Races"][0]["Results"]
+intents = discord.Intents.default()
+intents.members = True
 
-    # Create a message to post in the Discord channel
-    message = "**F1 Race Results**\n"
-    for result in results:
-        message += f"{result['Driver']['familyName']}: {result['position']}\n"
+client = discord.Client(intents=intents)
 
-    # Get the channel where you want to post the message
-    channel = client.get_channel("1104877246994460843")
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user.name} ({client.user.id})")
 
-    # Post the message in the channel
-    channel.send(message)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-# Set up a client to interact with the Discord API
-client = discord.Client()
+    if message.content.startswith("!f1result"):
+        result = get_race_result()
+        await message.channel.send(result)
 
-# Define a background task that runs every minute and posts the race results
-@tasks.loop(minutes=1)
-async def post_results_task():
-    await client.wait_until_ready()
-    await post_race_results()
+def get_race_result():
+    response = requests.get(API_URL)
 
-# Start the background task
-post_results_task.start()
+    if response.status_code != 200:
+        return "Error fetching F1 race result"
 
-# Run the bot
-client.run(TOKEN)
+    data = response.json()
+    race = data["MRData"]["RaceTable"]["Races"][0]
+    result = f"Results for {race['raceName']}, {race['Circuit']['circuitName']}\n\n"
+
+    for driver in race["Results"]:
+        position = driver["position"]
+        name = driver["Driver"]["givenName"] + " " + driver["Driver"]["familyName"]
+        team = driver["Constructor"]["name"]
+        time = driver["Time"]["time"] if "Time" in driver else "+0"
+
+        result += f"{position}. {name} - {team} ({time})\n"
+
+    return result
+
+client.run("MTEwNDg5NzI0NDg5NDg1NTIxOQ.GHZZxK.pU5IjWbfS1P7zonx-VW6k8idqKAFFD3bJWk13g")
